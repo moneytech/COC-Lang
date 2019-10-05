cf9c2f947dac4f24c32cd6c5ddc66459fc12d63c
I-am-Sagar <sagar.udasi.official@gmail.com>
2019-10-05 22:16:17 +0530

    Added basic implementation of Lexing.

--- coc-lang/coc.c
+++ coc-lang/coc.c
@@ -7,7 +7,6 @@
 #define MAX(x, y) (((x) >= (y)) ? (x) : (y))
 
 // Custom malloc and realloc for Stretchy 
-
 void *xrealloc(void* ptr, size_t num_bytes) {
 	ptr = realloc(ptr, num_bytes);
 	if (!ptr) {
@@ -37,8 +36,8 @@ typedef struct BufHdr {
 #define buf__fits(b, n) (buf_len(b) + (n) <= buf_cap(b))
 #define buf__fit(b, n) (buf__fits(b, n) ? 0 : ((b) = buf__grow((b), buf_len(b) + (n), sizeof(*(b)))))
 
-#define buf_len(b) ( (b) ? (buf__hdr(b)->len) : 0)
-#define buf_cap(b) ( (b) ? (buf__hdr(b)->cap) : 0)
+#define buf_len(b) ((b) ? (buf__hdr(b)->len) : 0)
+#define buf_cap(b) ((b) ? (buf__hdr(b)->cap) : 0)
 #define buf_push(b, x) ( buf__fit(b, 1), (b)[buf_len(b)] = (x), buf__hdr(b)->len++ )
 #define buf_free(b) ((b) ? free(buf__hdr(b)) : 0 )
 
@@ -69,7 +68,150 @@ void buf_test() {
     buf_free(mybuf);
 }
 
+// Let's start with, 
+// Lexing: translating char stream to token stream
+// eg. 1234(x+y) translates into '1234', '(', 'x', '+',  'y', ')'.
+
+typedef enum TokenKind {
+	TOKEN_INT = 128, 
+	TOKEN_NAME,
+	// ...
+} TokenKind;
+
+typedef struct Token {
+	TokenKind kind;
+	union {
+		uint64_t val;
+		struct {
+			const char *start;
+			const char *end;
+		};
+	};
+	// ...
+} Token;
+
+Token token;
+const char* stream;
+
+void next_token() {
+	switch (*stream) {
+		case '0':
+		case '1':
+		case '2':
+		case '3':
+		case '4':
+		case '5':
+		case '6':
+		case '7':
+		case '8':
+		case '9': { 
+			// TOKEN_INT
+			uint64_t val = 0;
+			while(isdigit(*stream)) {
+				val *= 10;
+				val += *stream++ - '0';
+			}
+			token.kind = TOKEN_INT;
+			token.val = val;
+			break;
+		}
+
+		case 'a':
+		case 'b':
+		case 'c':
+		case 'd':
+		case 'e':
+		case 'f':
+		case 'g':
+		case 'h':
+		case 'i':
+		case 'j':
+		case 'k':
+		case 'l':
+		case 'm':
+		case 'n':
+		case 'o':
+		case 'p':
+		case 'q':
+		case 'r':
+		case 's':
+		case 't':
+		case 'u':
+		case 'v':
+		case 'w':
+		case 'x':
+		case 'y':
+		case 'z':
+		case 'A':
+		case 'B':
+		case 'C':
+		case 'D':
+		case 'E':
+		case 'F':
+		case 'G':
+		case 'H':
+		case 'I':
+		case 'J':
+		case 'K':
+		case 'L':
+		case 'M':
+		case 'N':
+		case 'O':
+		case 'P':
+		case 'Q':
+		case 'R':
+		case 'S':
+		case 'T':
+		case 'U':
+		case 'V':
+		case 'W':
+		case 'X':
+		case 'Y':
+		case 'Z':
+		case '_': {
+			// TOKEN NAME
+			const char *start = stream++;
+			while (isalnum(*stream) || *stream == '_') {
+				stream++;
+			}
+			token.kind = TOKEN_NAME;
+			token.start = start;
+			token.end = stream;
+			break;
+		}
+		default:
+			token.kind = *stream++;
+			break;
+    }
+}
+
+void print_token(Token token) {
+    switch (token.kind) {
+		case TOKEN_INT:
+			printf("TOKEN INT: %llu\n", token.val);
+			break;
+		case TOKEN_NAME:
+			printf("TOKEN NAME: %.*s\n", (int)(token.end - token.start), token.start);
+			break;
+		default:
+			printf("TOKEN '%c'\n", token.kind);
+			break;
+    }
+}
+
+void lex_test() {
+	char* source = "+()_HELLO1,1234+FOO!994";
+	stream = source;
+	next_token();
+	while (token.kind) {
+		print_token(token);
+		next_token();
+	}
+}
+
+/* ---------------- MAIN DRIVER CODE ----------------- */
 int main(int argc, char** argv) {
 	buf_test();
+	lex_test();
 	return 0;
 }
