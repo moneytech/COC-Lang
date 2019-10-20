1cf51975a014220c7c8a088c35d94c654b94b8f0
I-am-Sagar <sagar.udasi.official@gmail.com>
2019-10-21 04:30:08 +0530

    Code clean up. Compiler not transpiling in this stage.

--- coc-lang/common.c
+++ coc-lang/common.c
@@ -73,28 +73,24 @@ char *read_file(const char *path) {
     return buf;
 }
 
-bool write_file(const char *path, const char *buf, size_t len) {
+int write_file(const char *path, const char *buf, size_t len) {
     FILE *file = fopen(path, "w");
     if (!file) {
         return false;
     }
-    bool result = false;
-    if (fwrite(buf, len, 1, file) != 1) {
-        goto done;
-    }
-    result = true;
-done:
+    size_t n = fwrite(buf, len, 1, file);
     fclose(file);
-    return result;
+    return n == 1;
 }
 
 const char *get_ext(const char *path) {
-    for (const char *ptr = path + strlen(path); ptr != path; ptr--) {
-        if (ptr[-1] == '.') {
-            return ptr;
+    const char *ext = NULL;
+    for (; *path; path++) {
+        if (*path == '.') {
+            ext = path + 1;
         }
     }
-    return NULL;
+    return ext;
 }
 
 char *replace_ext(const char *path, const char *new_ext) {
@@ -243,69 +239,62 @@ uint64_t ptr_hash(void *ptr) {
 }
 
 uint64_t str_hash(const char *str, size_t len) {
-    uint64_t fnv_init = 14695981039346656037ull;
-    uint64_t fnv_mul = 1099511628211ull;
-    uint64_t h = fnv_init;
+    uint64_t x = 0xcbf29ce484222325ull;
     for (size_t i = 0; i < len; i++) {
-        h ^= str[i];
-        h *= fnv_mul;
+        x ^= str[i];
+        x *= 0x100000001b3ull;
+        x ^= x >> 32;
     }
-    return h;
+    return x;
 }
 
-typedef struct MapEntry {
-    void *key;
-    void *val;
-    uint64_t hash;
-} MapEntry;
-
 typedef struct Map {
-    MapEntry *entries;
+    void **keys;
+    void **vals;
     size_t len;
     size_t cap;
 } Map;
 
-void *map_get_hashed(Map *map, void *key, uint64_t hash) {
+void *map_get(Map *map, void *key) {
     if (map->len == 0) {
         return NULL;
     }
     assert(IS_POW2(map->cap));
-    uint32_t i = (uint32_t)(hash & (map->cap - 1));
+    uint32_t i = (uint32_t)ptr_hash(key);
     assert(map->len < map->cap);
     for (;;) {
-        MapEntry *entry = map->entries + i;
-        if (entry->key == key) {
-            return entry->val;
-        } else if (!entry->key) {
+        i &= map->cap - 1;
+        if (map->keys[i] == key) {
+            return map->vals[i];
+        } 
+        else if (!map->keys[i]) {
             return NULL;
         }
         i++;
-        if (i == map->cap) {
-            i = 0;
-        }
     }
     return NULL;
 }
 
-void **map_put_hashed(Map *map, void *key, void *val, uint64_t hash);
+void **map_put(Map *map, void *key, void *val);
 
 void map_grow(Map *map, size_t new_cap) {
     new_cap = MAX(16, new_cap);
     Map new_map = {
-        .entries = xcalloc(new_cap, sizeof(MapEntry)),
-        .cap = new_cap
+        .keys = xcalloc(new_cap, sizeof(void *)),
+        .vals = xmalloc(new_cap * sizeof(void *)),
+        .cap = new_cap,
     };
     for (size_t i = 0; i < map->cap; i++) {
-        MapEntry *entry = map->entries + i;
-        if (entry->key) {
-            map_put_hashed(&new_map, entry->key, entry->val, entry->hash);
+        if (map->keys[i]) {
+            map_put(&new_map, map->keys[i], map->vals[i]);
         }
     }
-    free(map->entries);
+    free(map->keys);
+    free(map->vals);
     *map = new_map;
 }
 
-void **map_put_hashed(Map *map, void *key, void *val, uint64_t hash) {
+void **map_put(Map *map, void *key, void *val) {
     assert(key);
     assert(val);
     if (2*map->len >= map->cap) {
@@ -313,32 +302,20 @@ void **map_put_hashed(Map *map, void *key, void *val, uint64_t hash) {
     }
     assert(2*map->len < map->cap);
     assert(IS_POW2(map->cap));
-    uint32_t i = (uint32_t)(hash & (map->cap - 1));
+    uint32_t i = (uint32_t)ptr_hash(key);
     for (;;) {
-        MapEntry *entry = map->entries + i;
-        if (!entry->key) {
+        if (!map->keys[i]) {
             map->len++;
-            entry->key = key;
-            entry->val = val;
-            entry->hash = hash;
-            return &entry->val;
-        } else if (entry->key == key) {
-            entry->val = val;
-            return &entry->val;
+            map->keys[i] = key;
+            map->vals[i] = val;
+            return map->vals + i;
+        } 
+        else if (map->keys[i] == key) {
+            map->vals[i] = val;
+            return map->vals + i;
         }
         i++;
-        if (i == map->cap) {
-            i = 0;
-        }
     }
-}
-
-void **map_put(Map *map, void *key, void *val) {
-    return map_put_hashed(map, key, val, ptr_hash(key));
-}
-
-void *map_get(Map *map, void *key) {
-    return map_get_hashed(map, key, ptr_hash(key));
 }
 
 void map_test(void) {
@@ -361,25 +338,26 @@ typedef struct Intern {
     char str[];
 } Intern;
 
-Arena str_arena;
+Arena intern_arena;
 
 Map interns;
 
 const char *str_intern_range(const char *start, const char *end) {
     size_t len = end - start;
-    uint64_t hash = str_hash(start, len) | 1;
-    Intern *intern = map_get_hashed(&interns, (void *)hash, hash);
+    uint64_t hash = str_hash(start, len);
+    void *key = (void *)(hash ? hash : 1);
+    Intern *intern = map_get(&interns, key);
     for (Intern *it = intern; it; it = it->next) {
         if (it->len == len && strncmp(it->str, start, len) == 0) {
             return it->str;
         }
     }
-    Intern *new_intern = arena_alloc(&str_arena, offsetof(Intern, str) + len + 1);
+    Intern *new_intern = arena_alloc(&intern_arena, offsetof(Intern, str) + len + 1);
     new_intern->len = len;
     new_intern->next = intern;
     memcpy(new_intern->str, start, len);
     new_intern->str[len] = 0;
-    map_put_hashed(&interns, (void *)hash, new_intern, hash);
+    map_put(&interns, key, new_intern);
     return new_intern->str;
 }
 
